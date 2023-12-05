873478b643cafe44be15f1cfd0d69cfd79bfe7e4
Enby <d20210318080@unifei.edu.br>
2023-12-04 22:12:20 -0300

    home_page

--- skybooker/lib/view/pages/home_page.dart
+++ skybooker/lib/view/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:skybooker/model/travel_model.dart';
 import 'package:skybooker/view/widgets/app_navigation_bar.dart';
 import 'package:skybooker/view/widgets/travel_card.dart';
 import 'package:skybooker/view/widgets/web_navigation_bar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 import '../widgets/responsive_widget.dart';
 
@@ -13,6 +14,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
+    final List<String> landscapeImages = [
+      'https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1501785888041-af3ef285b470?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1511576661531-b34d7da5d0bb?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ];
+
     return Scaffold(
       appBar: ResponsiveWidget.isSmallScreen(context)
           ? PreferredSize(
@@ -26,13 +34,36 @@ class HomePage extends StatelessWidget {
               preferredSize: Size(screenSize.width, 1000),
               child: const WebNavigationBar(),
             ),
-      body: Container( alignment: Alignment.center,
-      child: Column(mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text('Bem vindo a Home Page', style: TextStyle(fontSize: 20,),)
-        ],
-      )
-    ),
+      body: CarouselSlider(
+        options: CarouselOptions(
+          height: 400.0,
+          enlargeCenterPage: true,
+          autoPlay: true,
+          aspectRatio: 2.0,
+          autoPlayCurve: Curves.fastOutSlowIn,
+          enableInfiniteScroll: true,
+          autoPlayAnimationDuration: Duration(milliseconds: 800),
+          viewportFraction: 0.8,
+        ),
+        items: landscapeImages.map((url) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: NetworkImage(url),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            },
+          );
+        }).toList(),
+      ),
     );
   }
 }
